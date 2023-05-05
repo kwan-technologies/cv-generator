8852066f0963d2bdf302f6aad7ae21e651fea22d
kalujja <kalujja@kalujjas-MacBook-Pro.local>
2023-05-05 17:27:04 +0300

    Refactor wording

--- src/pages/authentication/CheckCv.js
+++ src/pages/authentication/CheckCv.js
@@ -12,7 +12,7 @@ const CheckCv = () => (
         <Grid container spacing={3}>
             <Grid item xs={12}>
                 <Stack direction="row" justifyContent="center" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-                    <Typography variant="h3">Open CV - Header</Typography>
+                    <Typography variant="h3">Open CV</Typography>
                 </Stack>
             </Grid>
             <Grid item xs={12}>
